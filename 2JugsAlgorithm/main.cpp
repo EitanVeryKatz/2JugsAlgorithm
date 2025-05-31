1ee2bc200e3ddba06a14d68f6bc8a2ec535637e3
EitanKatz <eitanka@mta.ac.il>
2025-05-31 12:33:04 +0300

    added input check and propper main

--- 2JugsAlgorithm/main.cpp
+++ 2JugsAlgorithm/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include "JugsGraphSolver.h"
 #include "JugsDynamicSolver.h"
+#include <stdlib.h>
+#define FirstAlgotrithm 1
+#define SecondAlgotrithm 2
 
 void main() 
 {
@@ -16,10 +19,12 @@ void main()
 	std::cin >> T;
 
 
-	
-	JugsGraphSolver g(L, S, T);
-	g.Solve(W);
-	JugsDynamicSolver d(L, S, T);
-	d.Solve(W);
+	if (L <= 0 || S <= 0 || W < 0 || T < 0 || (A != FirstAlgotrithm && A != SecondAlgotrithm)||T>1){
+		std::cout << "invalid input.";
+		exit(1);
+	}
+	JugsProblemSolver* solver = nullptr;
+	solver = (A == FirstAlgotrithm ? (JugsProblemSolver*)new JugsGraphSolver(L, S, T) : (JugsProblemSolver*)new JugsDynamicSolver(L, S, T));
+	solver->Solve(W);
 
 }
